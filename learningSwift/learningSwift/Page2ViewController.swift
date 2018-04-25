e43c21357cf391595b4a827fcf20c70a74767491
Caroline Rayez <caro.rayez@gmail.com>
2018-04-25 14:42:54 +0200

    affichage liste bouton

--- learningSwift/learningSwift/Page2ViewController.swift
+++ learningSwift/learningSwift/Page2ViewController.swift
@@ -10,15 +10,6 @@ import UIKit
 
 class Page2ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath as IndexPath)
-        
-        cell.textLabel?.text = self.itemsToLoad[indexPath.row]
-        
-        return cell
-    }
-    
     let color = UIColor.blue
     
     var itemsToLoad: [String] = ["One", "Two", "Three"]
@@ -36,7 +27,7 @@ class Page2ViewController: UIViewController, UITableViewDataSource, UITableViewD
 //        let screenWidth = view
 //        let screenHeight = view
         
-        myTableView.separatorColor = UIColor.orange
+        myTableView.separatorColor = UIColor.lightGray
         myTableView.backgroundColor = color
         myTableView.translatesAutoresizingMaskIntoConstraints = false
         // myTableView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
@@ -49,39 +40,43 @@ class Page2ViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         tableContraint()
         
-        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return itemsToLoad.count
+        
     }
     
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         print("User selected table row \(indexPath.row) and item \(itemsToLoad[indexPath.row])")
         
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        print(indexPath.row)
+        
+        cell.textLabel?.text = self.itemsToLoad[indexPath.row]
+        
+        return cell
+        
+    }
+    
     func tableContraint(){
         
-        var ecart = -100
+        //placement sur X et Y
+        myTableView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: CGFloat(-200)).isActive = true
+        myTableView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         
-        for tour in 0...1000 {
-                
-            //placement sur X et Y
-            myTableView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: CGFloat(ecart)).isActive = true
-            myTableView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        //taille
+        myTableView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.25).isActive = true
+        myTableView.heightAnchor.constraint(equalToConstant: 400).isActive = true
         
-            //taille
-            myTableView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.25).isActive = true //constraint(equalTo: self.view.widthAnchor).isActive = true
-            myTableView.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        
-            ecart = ecart + 30
-            var tour = tour + 1
-        }
     }
     
 }
