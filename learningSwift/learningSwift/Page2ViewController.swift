275f806ec102cdc589624425daef1dbaf60d6819
Caroline Rayez <caro.rayez@gmail.com>
2018-04-25 15:13:05 +0200

    on click change background color in page 2

--- learningSwift/learningSwift/Page2ViewController.swift
+++ learningSwift/learningSwift/Page2ViewController.swift
@@ -12,7 +12,7 @@ class Page2ViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     let color = UIColor.blue
     
-    var itemsToLoad: [String] = ["One", "Two", "Three"]
+    var itemsToLoad: [String] = ["Green", "Yellow", "Magenta", "Cyan", "White", "Black", "Brown", "Gray", "Red"]
     
     var myTableView: UITableView = UITableView()
     //var itemsToLoad: [String] = [self.table]
@@ -27,8 +27,7 @@ class Page2ViewController: UIViewController, UITableViewDataSource, UITableViewD
 //        let screenWidth = view
 //        let screenHeight = view
         
-        myTableView.separatorColor = UIColor.lightGray
-        myTableView.backgroundColor = color
+        myTableView.separatorColor = UIColor.black
         myTableView.translatesAutoresizingMaskIntoConstraints = false
         // myTableView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
         myTableView.dataSource = self
@@ -50,6 +49,47 @@ class Page2ViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            
+            var click = indexPath.row
+            
+                //green
+            if click == 0 {
+                self.view.backgroundColor = UIColor.green
+            }
+                //yellow
+            else if click == 1 {
+                self.view.backgroundColor = UIColor.yellow
+            }
+                //magenta
+            else if click == 2 {
+                self.view.backgroundColor = UIColor.magenta
+            }
+                //cyan
+            else if click == 3 {
+                self.view.backgroundColor = UIColor.cyan
+            }
+                //white
+            else if click == 4 {
+                self.view.backgroundColor = UIColor.white
+            }
+                //black
+            else if click == 5 {
+                self.view.backgroundColor = UIColor.black
+            }
+                //brown
+            else if click == 6 {
+                self.view.backgroundColor = UIColor.brown
+            }
+                //gray
+            else if click == 7 {
+                self.view.backgroundColor = UIColor.gray
+            }
+                //red
+            else if click == 8 {
+                self.view.backgroundColor = UIColor.red
+            }
+            
+        
         
         print("User selected table row \(indexPath.row) and item \(itemsToLoad[indexPath.row])")
         
@@ -70,12 +110,12 @@ class Page2ViewController: UIViewController, UITableViewDataSource, UITableViewD
     func tableContraint(){
         
         //placement sur X et Y
-        myTableView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: CGFloat(-200)).isActive = true
+        myTableView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: CGFloat(100)).isActive = true
         myTableView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         
         //taille
         myTableView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.25).isActive = true
-        myTableView.heightAnchor.constraint(equalToConstant: 400).isActive = true
+        myTableView.heightAnchor.constraint(equalToConstant: 600).isActive = true
         
     }
     
