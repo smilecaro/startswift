dc336e452699eb0d464f462b6dd299fa399bf2eb
Caroline Rayez <caro.rayez@gmail.com>
2018-04-25 12:13:04 +0200

    transformation 1 bouton

--- learningSwift/learningSwift/Page2ViewController.swift
+++ learningSwift/learningSwift/Page2ViewController.swift
@@ -21,26 +21,33 @@ class Page2ViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     let color = UIColor.blue
     
+    var itemsToLoad: [String] = ["One", "Two", "Three"]
+    
     var myTableView: UITableView = UITableView()
-    var itemsToLoad: [String] = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+    //var itemsToLoad: [String] = [self.table]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = color
         
-        let screenSize: CGRect = UIScreen.main.bounds
+//        let screenSize: CGRect = UIScreen.main.bounds
+//
+//        let screenWidth = view
+//        let screenHeight = view
         
-        let screenWidth = screenSize.width
-        let screenHeight = screenSize.height
-        
-        myTableView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+        myTableView.separatorColor = UIColor.orange
+        myTableView.backgroundColor = color
+        myTableView.translatesAutoresizingMaskIntoConstraints = false
+        // myTableView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
         myTableView.dataSource = self
         myTableView.delegate = self
         
         myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
         
         self.view.addSubview(myTableView)
+        
+        tableContraint()
         
         
     }
@@ -53,6 +60,28 @@ class Page2ViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
+        
         print("User selected table row \(indexPath.row) and item \(itemsToLoad[indexPath.row])")
+        
     }
+    
+    func tableContraint(){
+        
+        var ecart = -100
+        
+        for tour in 0...1000 {
+                
+            //placement sur X et Y
+            myTableView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: CGFloat(ecart)).isActive = true
+            myTableView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+            //taille
+            myTableView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.25).isActive = true //constraint(equalTo: self.view.widthAnchor).isActive = true
+            myTableView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+            ecart = ecart + 30
+            var tour = tour + 1
+        }
+    }
+    
 }
